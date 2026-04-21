cb0cd487ceda58db9f6d0181a5e87baa9b1dbc2b
taoufik03-creator <taoufikbouhamla6@gmail.com>
2026-04-21 22:49:14 +0100

    Update DATABASE_URL environment variable for database

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,7 @@ import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("postgresql://postgres:tmAxyDjvEALAJtBrKLppAzhwpCZeMiqP@postgres.railway.internal:5432/railway"))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
