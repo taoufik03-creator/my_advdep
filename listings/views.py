556eeedeaee588bc5fc6f1d8c79f671d942edabc
taoufik03-creator <taoufikbouhamla6@gmail.com>
2026-04-21 14:57:22 +0100

    Add PropertyListView to display properties

--- listings/views.py
+++ listings/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Property
+
 from django.http import HttpResponse
+from django.views.generic import *
 
 def home(request):
     return HttpResponse("UrbanHub is running 🚀")
+
+class PropertyListView(ListView):
+    template_name="property_list.html"
+    queryset=Property.objects.all()
