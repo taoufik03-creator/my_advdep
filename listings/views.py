05549819f708ebce64602cbadeb56e96cee20968
taoufik03-creator <taoufikbouhamla6@gmail.com>
2026-04-21 15:09:33 +0100

    Update views.py

--- listings/views.py
+++ listings/views.py
@@ -5,11 +5,9 @@ from django.shortcuts import render
 from .models import Property
 
 from django.http import HttpResponse
-from django.views.generic import *
+
 
 def home(request):
     return HttpResponse("UrbanHub is running 🚀")
 
-class PropertyListView(ListView):
-    template_name="property_list.html"
-    queryset=Property.objects.all()
+
